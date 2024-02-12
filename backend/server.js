1f5075d738435d33b8c52a6af099f721732f6f22
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-02-12 17:12:14 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// const usersRouter = require("./routes/users");
+// const vendorsRouter = require("./routes/vendors");
+// const itemsRouter = require("./routes/items");
+// const ordersRouter = require("./routes/orders");
+// const transactionsRouter = require("./routes/transactions");
+
+// app.use("/users", usersRouter);
+// app.use("/vendors", vendorsRouter);
+// app.use("/items", itemsRouter);
+// app.use("/orders", ordersRouter);
+// app.use("/transactions", transactionsRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
